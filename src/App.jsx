ec23a6b4a95b70350bde5f3376995f427ecf6e3d
Vasanth-Raja <darkdragn66@gmail.com>
2023-09-14 15:55:56 +0530

    firstcommit-for-nike

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import {CustomerReviews, Hero, Footer, PopularProducts, Services, Subscribe, SpecialOffer,SuperQuality} from "./sections/comp"
+import Nav from "./components/Nav";
+const App=()=>{
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
+};
+export default App;
